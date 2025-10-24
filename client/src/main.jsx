b3fab0e4af409438500eb2e7bfed0b2f77dc4bc5
PranaySAG <pranayshinde929@gmail.com>
2025-10-24 13:24:44 +0530

    lib and route setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import AllBlogs from './views/AllBlogs'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<AllBlogs />} />
+      <Route path="/new" element={<NewBlog />} />
+      <Route path="/edit/:id" element={<EditBlog />} />
+      <Route path="/blog/:slug" element={<ReadBlog />} />
+    </Routes>
+  </BrowserRouter>
+
 )
