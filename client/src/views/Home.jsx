08e9ee6859e9276cb2bf0347f50679f28e480ad3
Pranay Narendra Shinde <pranayshinde929@gmail.com>
2025-11-08 16:00:39 +0530

    Update Home.jsx

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -67,7 +67,7 @@ export default function Home() {
                                 <h4 className="font-serif text-xl mt-2 mb-2">{a.title}</h4>
                                 <p className="text-gray-700 text-sm mb-4">{a.excerpt}</p>
                                 <Link
-                                    to="/read-blog"
+                                    to="/blogs"
                                     className="inline-block text-sm border border-gray-800 text-gray-900 px-4 py-2 hover:bg-gray-900 hover:text-white transition-colors"
                                 >
                                     Read More
